e93117422816a803cda447e868f648c65a402318
Trinity John <trinityrtime@gmail.com>
2019-11-04 11:44:25 +0300

    in html editedrow for contact and social divs ;;full edit of js script file.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+lines (18 sloc)  455 Bytes
+ 
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $(".design-hidden").toggle();
+    $(".design-showing").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $(".development-hidden").toggle();
+    $(".development-showing").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable3").click(function() {
+    $(".product-hidden").toggle();
+    $(".product-showing").toggle();
+  });
+});
